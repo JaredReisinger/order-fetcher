573c48053e748a46a64a27a3abaee62ef3850226
Jared Reisinger <jaredreisinger@hotmail.com>
2019-07-03 16:54:13 -0700

    test: add stub test file to ensure all source files get coverage
    We're now using `c8`, which doesn't have the `--all` option that `nyc` does... but we can fake it by
    including a shim test file whose sole purpose is to import every single source file (that we care
    about).  Our apparent coverage has dropped, but that's good; it actually means we're getting a
    better picture of the overall coverage.

---
+++ test/all.test.js
@@ -0,0 +1,15 @@
+// This exists purely to bring in all files, since c8 doesn't yet support an
+// --all flag.
+
+// import '../lib/cli';
+import '../lib/helpers';
+
+import '../lib/commands/index';
+// import '../lib/commands/config';
+// import '../lib/commands/get';
+
+import '../lib/wc/wcutils';
+import '../lib/wc/WooClient';
+import '../lib/wc/WooCurrencies';
+import '../lib/wc/WooItem';
+import '../lib/wc/WooOrder';
